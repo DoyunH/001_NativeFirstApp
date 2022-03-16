0d4418baeaf30ce2d2f33278c5166612f03cded5
Doyun <doyun@talktomeinkorean.com>
2022-03-16 17:39:04 +0900

    feature : useState 연습

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, Button} from 'react-native';
+import Greeting from './components/Greeting';
+import Box from './components/Box';
 
 const App = () => {
+  const [visible, setVisible] = useState(true);
+  const onPress = () => {
+    setVisible(!visible);
+  };
   return (
-    <>
-      <View>
-        <Text>Hello React!</Text>
-      </View>
-    </>
+    <SafeAreaView>
+      <Greeting name={21} />
+      <Button title="toggle" onPress={onPress} />
+      {visible && (
+        <>
+          <Box rounded color="gray" />
+          <Box size="medium" />
+          <Box rounded size="large" color="#405196" />
+        </>
+      )}
+    </SafeAreaView>
   );
 };
 
